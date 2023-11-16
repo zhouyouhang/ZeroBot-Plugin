83eb5f6b0f376bbcce073d894e3e5271b2d96fe9
zhouyouhang <150726484+zhouyouhang@users.noreply.github.com>
2023-11-16 23:24:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -259,7 +259,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	// sus = append(sus, 2483988332)
-	// sus = append(sus, 87654321)
+	// sus = append(sus, 1179918813)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
